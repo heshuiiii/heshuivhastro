d35d0f72eb86d8ba0f0ab5b6e040e9b424e1f17e
heshuiiii <71701401+heshuiiii@users.noreply.github.com>
2025-08-05 01:29:36 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -16,7 +16,7 @@ export default {
   // Cover 网站缩略图
   Cover: '/assets/images/banner/favicon.png',
   // 网站侧边栏公告 (不填写即不开启)
-  Tips: '<p>欢迎光临我的博客 🎉</p><p>这里会分享我的日常和学习中的收集、整理及总结，希望能对你有所帮助:) 💖</p>',
+  Tips: '',
   // 首页打字机文案列表
   // TypeWriteList: [
   //   '不曾与你分享的时间,我在进步.',
