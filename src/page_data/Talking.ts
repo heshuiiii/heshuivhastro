4931f82b36431d99e045b8f42c8903393d2121a8
heshuiiii <71701401+heshuiiii@users.noreply.github.com>
2025-08-28 17:35:19 +0800

    Add new talking: 暴雨预警么，有点意思...

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -2,6 +2,11 @@ export default {
   api: '',
   data: [
     {
+      "date": "2025-08-28 17:35:17",
+      "tags": [],
+      "content": "暴雨预警么，有点意思"
+    },
+    {
       "date": "2025-08-28 03:42:42",
       "tags": [],
       "content": "早点完蛋算了哈哈哈哈哈<p class=\"vh-img-flex\"><img src=\"https://picup.ikuyo.de/i/2025/08/28/1000003932.jpg\"></p>"
