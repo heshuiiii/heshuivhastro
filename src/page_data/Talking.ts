4121817a348ba60cc5169ace5c3f14c44bb863b7
heshuiiii <71701401+heshuiiii@users.noreply.github.com>
2025-09-29 00:56:54 +0800

    Add new talking: ...

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -2,6 +2,11 @@ export default {
   api: '',
   data: [
     {
+      "date": "2025-09-29 00:56:52",
+      "tags": [],
+      "content": "<p class=\"vh-img-flex\"><img src=\"https://picup.ikuyo.de/i/2025/09/29/1759078487388.jpg\"></p><p class=\"vh-img-flex\"><img src=\"https://picup.ikuyo.de/i/2025/09/29/1759078487336.jpg\"></p><p class=\"vh-img-flex\"><img src=\"https://picup.ikuyo.de/i/2025/09/29/1759078487241.jpg\"></p><p class=\"vh-img-flex\"><img src=\"https://picup.ikuyo.de/i/2025/09/29/1759078487290.jpg\"></p>"
+    },
+    {
       "date": "2025-09-29 00:54:00",
       "tags": [],
       "content": "想找下高中的照片的，翻了一下一张也没有，sad"
